13ae97caa9dd7330e5df10d55d7125887684036d
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-08 18:01:17 +0530

    Completed Scope of Beans

--- src/main/java/com/anurag/spring/innerbeans/Test.java
+++ src/main/java/com/anurag/spring/innerbeans/Test.java
@@ -8,6 +8,11 @@ public class Test {
 
         Employee emp = (Employee)ctx.getBean("employee");
         System.out.println(emp);
+        System.out.println("Hash Code: "+emp.hashCode());
+
+        Employee emp2 = (Employee)ctx.getBean("employee");
+        System.out.println(emp2);
+        System.out.println("Hash Code: "+emp2.hashCode());
 
         ctx.close();
     }
