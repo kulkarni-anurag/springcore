f65ae47804f9d6744ca1606b8f4501f925ddd1fd
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-09 10:28:02 +0530

    Added index attribute to constructor injection

--- src/main/java/com/anurag/spring/constructorinjection/ambiguity/Addition.java
+++ src/main/java/com/anurag/spring/constructorinjection/ambiguity/Addition.java
@@ -2,15 +2,9 @@ package com.anurag.spring.constructorinjection.ambiguity;
 
 public class Addition {
 
-    Addition(double a, double b){
-        System.out.println("Inside constructor double");
-    }
-
-    Addition(int a, int b){
-        System.out.println("Inside constructor int");
-    }
-
-    Addition(String a, String b){
-        System.out.println("Inside constructor String");
+    Addition(int a, double b){
+        System.out.println("Inside constructor");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
     }
 }
